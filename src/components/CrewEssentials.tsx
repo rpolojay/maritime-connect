f4155107b44a1ca3ab01f666019805ba31c036b6
rpolojay <rpolo@selectivecrew.com>
2026-01-12 19:22:06 -0500

    Create CrewEssentials component

---
+++ src/components/CrewEssentials.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+export default function CrewEssentials() {
+  return (
+    <div className="bg-blue-50 p-6 rounded-lg border border-blue-200">
+      <h3 className="text-lg font-semibold text-blue-900 mb-4">Essential Crew Items</h3>
+      <ul className="list-disc list-inside space-y-2 text-blue-800">
+        <li>Valid passport and visas</li>
+        <li>CDC seafarer health documentation</li>
+        <li>Professional certifications (STCW, etc.)</li>
+        <li>Medical exam results</li>
+        <li>Proper uniforms and shoes</li>
+        <li>Personal toiletries and medications</li>
+      </ul>
+    </div>
+  );
+}
