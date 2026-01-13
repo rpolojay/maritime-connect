7e7cc4a2c5cb692d61c71b46ce95f3d39594ab3a
rpolojay <rpolo@selectivecrew.com>
2026-01-12 19:16:29 -0500

    Import CrewEssentials component in BlogPostPage

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -9,6 +9,7 @@ import { User, Calendar } from 'lucide-react';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Badge } from '@/components/ui/badge';
 import Link from 'next/link';
+import { CrewEssentials } from '@/components/CrewEssentials';
 
 
 export default function BlogPostPage() {
