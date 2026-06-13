b82674c28d5c173196dd701e447a533325776705
rpolojay <rpolo@selectivecrew.com>
2026-06-12 22:26:39 -0500

    Add redirects in next.config.js

---
+++ next.config.js
@@ -0,0 +1,33 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      { source: '/index.php/contact', destination: '/contact', permanent: true },
+      { source: '/index.php/about', destination: '/about', permanent: true },
+      { source: '/about-2', destination: '/about', permanent: true },
+      { source: '/apply-now', destination: '/apply', permanent: true },
+      { source: '/apply-now-2', destination: '/apply', permanent: true },
+      { source: '/home', destination: '/', permanent: true },
+      { source: '/jobs/gcc-ca-1', destination: '/jobs', permanent: true },
+      { source: '/jobs/sb-rsm-1', destination: '/jobs', permanent: true },
+      { source: '/jobs/cunard-receptionist-1', destination: '/jobs', permanent: true },
+      { source: '/jobs/cunard-sommelier-1', destination: '/jobs', permanent: true },
+      { source: '/jobs/gcc-drw-1', destination: '/jobs', permanent: true },
+      { source: '/jobs/gcc-co-1', destination: '/jobs', permanent: true },
+      { source: '/jobs/po-bw-pc-1', destination: '/jobs', permanent: true },
+      { source: '/jobs/po-aw-pc-1', destination: '/jobs', permanent: true },
+      { source: '/jobs/11', destination: '/jobs', permanent: true },
+      { source: '/job/cruise-ship-retail-sales-manager', destination: '/jobs', permanent: true },
+      { source: '/job/cruise-ship-retail-sales-associate', destination: '/jobs', permanent: true },
+      { source: '/job/cruise-ship-assistant-retail-sales-manager', destination: '/jobs', permanent: true },
+      { source: '/job/cabin-attendant-cabin-steward-grand-circle-cruise-line-river-ships', destination: '/jobs', permanent: true },
+      { source: '/retail-jobs', destination: '/jobs', permanent: true },
+      { source: '/hospitality-jobs', destination: '/jobs', permanent: true },
+      { source: '/cruise-ship-photographer', destination: '/cruise-ship-photographer-jobs', permanent: true },
+      { source: '/reviews', destination: '/about', permanent: true },
+      { source: '/employers/cunard-line', destination: '/for-employers', permanent: true },
+    ]
+  },
+}
+
+module.exports = nextConfig
